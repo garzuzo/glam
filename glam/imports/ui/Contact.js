d4211274b57ad1b4331420e63bbb155c16b75bf0
garzuzo <garzuxo@gmail.com>
2019-06-25 00:39:14 -0500

    fix email api

--- glam/imports/ui/Contact.js
+++ glam/imports/ui/Contact.js
@@ -13,23 +13,26 @@ export default class Contact extends Component {
         this.state = {
             startDate: new Date(),
             endDate: new Date(),
-            reservePicker: [],
+        
             showReserve: false,
-            name:"",
-            phone:"",
-            totalPeople:"",
-            comments:""
+            name: "",
+            phone: "",
+            totalPeople: "",
+            comments: ""
         }
         this.handleReserve = this.handleReserve.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleStartDate = this.handleStartDate.bind(this);
+        this.handleEndDate = this.handleEndDate.bind(this);
     }
 
 
-    handleEmail(){
+    handleEmail() {
 
-let subject=this.state.name+" tiene una duda para Glam Hoteles";
-let message="nombre:"+this.state.name+"\n telefono:"+this.state.phone+"\n correo:"+this.state.email+"\n número de personas:"+totalPeople+"Fecha de reserva:"+this.state.startDate+"-"+this.state.endDate+"\n Comentarios:"+this.state.message;
-        
-        Meteor.call('sendEmail',"garzuxo@gmail.com", this.state.email, subject, message);
+        let subject = this.state.name + " tiene una duda para Glam Hoteles";
+        let message = "nombre:" + this.state.name + "\n telefono:" + this.state.phone + "\n correo:" + this.state.email + "\n número de personas:" + totalPeople + "Fecha de reserva:" + this.state.startDate + "-" + this.state.endDate + "\n Comentarios:" + this.state.message;
+
+        Meteor.call('sendEmail', "garzuxo@gmail.com", this.state.email, subject, message);
     }
     handleReserve() {
 
@@ -42,26 +45,22 @@ let message="nombre:"+this.state.name+"\n telefono:"+this.state.phone+"\n correo
             [event.target.name]: event.target.value
         });
     }
-    componentDidMount() {
 
-        this.state.reservePicker.push(
-            <div id="reserve" key="reserve">
-                <div className="">
-                    <label htmlFor="datepickerStart"><T>Common.contact.sinceDate</T></label>
-                    <DatePicker id="datepickerStart" className="form-control" value={this.state.startDate}  onChange={this.handleOnChange.bind(this)} required />
-                </div>
-                <div className="">
-                    <label htmlFor="datepickerEnd"><T>Common.contact.untilDate</T></label>
-                    <DatePicker id="datepickerEnd" className="form-control" value={this.state.endDate} onChange={this.handleOnChange.bind(this)} required />
-                </div>
-            </div>
-        );
-
+    handleStartDate(startDate) {
+        this.setState({
+            startDate: startDate
+        });
     }
+    handleEndDate(endDate) {
+        this.setState({
+            endDate: endDate
+        });
+    }
+   
 
 
     render() {
-        
+
 
 
 
@@ -70,32 +69,43 @@ let message="nombre:"+this.state.name+"\n telefono:"+this.state.phone+"\n correo
                 <h1><T>Common.contact.title</T></h1>
 
                 <form>
-                <div className="form-group">
+                    <div className="form-group">
                         <label htmlFor="name"><T>Common.contact.name</T></label>
-                        <input  className="form-control" id="name"  value={this.state.name} onChange={this.handleOnChange.bind(this)} required />
-                          </div>
-                          <div className="form-group">
+                        <input className="form-control" id="name" name="name" value={this.state.name} onChange={this.handleOnChange.bind(this)} required />
+                    </div>
+                    <div className="form-group">
                         <label htmlFor="phone"><T>Common.contact.phone</T></label>
-                        <input  className="form-control" id="phone"  value={this.state.phone} onChange={this.handleOnChange.bind(this)}  />
-                          </div>
+                        <input className="form-control" id="phone" name="phone" value={this.state.phone} onChange={this.handleOnChange.bind(this)} />
+                    </div>
                     <div className="form-group">
-                        <label htmlFor="exampleInputEmail1"><T>Common.contact.email</T></label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" value={this.state.email} onChange={this.handleOnChange.bind(this)}  required />
-                         </div>
+                        <label htmlFor="email"><T>Common.contact.email</T></label>
+                        <input type="email" className="form-control" id="email" name="email" value={this.state.email} onChange={this.handleOnChange.bind(this)} required />
+                    </div>
                     <div className="form-group">
-                        <label htmlFor="numberPeople"><T>Common.contact.totalPeople</T></label>
-                        <input type="number" className="form-control" id="numberPeople" value={this.state.totalPeople} onChange={this.handleOnChange.bind(this)}  />
+                        <label htmlFor="totalPeople"><T>Common.contact.totalPeople</T></label>
+                        <input type="number" className="form-control" id="totalPeople" name="totalPeople" min="1" value={this.state.totalPeople} onChange={this.handleOnChange.bind(this)} />
                     </div>
                     <br></br>
                     <div className="form-check">
-                        <input type="checkbox" data-toggle="collapse" className="form-check-input" id="exampleCheck1" href="#reserve"onChange={this.handleReserve} />
+                        <input type="checkbox" data-toggle="collapse" className="form-check-input" id="exampleCheck1" href="#reserve" onChange={this.handleReserve} />
                         <label className="form-check-label" htmlFor="exampleCheck1"><T>Common.contact.reserveDate</T></label>
                     </div>
-                    {this.state.showReserve ? this.state.reservePicker : null}
+                    {this.state.showReserve ?
+                        <div id="reserve" key="reserve">
+                            <div className="">
+                                <label htmlFor="startDate"><T>Common.contact.sinceDate</T></label>
+                                <DatePicker id="startDate" className="form-control" value={this.state.startDate} onChange={this.handleStartDate.bind(this)} required />
+                            </div>
+                            <div className="">
+                                <label htmlFor="endDate"><T>Common.contact.untilDate</T></label>
+                                <DatePicker id="endDate" className="form-control" value={this.state.endDate} onChange={this.handleEndDate.bind(this)} required />
+                            </div>
+                        </div>
+                        : null}
                     <br></br>  <br></br>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlTextarea1"><T>Common.contact.comments</T></label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={this.state.coments} onChange={this.handleOnChange.bind(this)} ></textarea>
+                        <label htmlFor="comments"><T>Common.contact.comments</T></label>
+                        <textarea className="form-control" id="comments" rows="3" name="comments" value={this.state.coments} onChange={this.handleOnChange.bind(this)} ></textarea>
                     </div>
                     <button type="submit" className="btn btn-success"><T>Common.contact.send</T></button>
                 </form>
