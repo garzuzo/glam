d25af42023d11ae0acfc141b56847e5fd887a209
garzuzo <garzuxo@gmail.com>
2019-06-24 23:26:07 -0500

    add email api

--- glam/imports/ui/Contact.js
+++ glam/imports/ui/Contact.js
@@ -14,29 +14,45 @@ export default class Contact extends Component {
             startDate: new Date(),
             endDate: new Date(),
             reservePicker: [],
-            showReserve: false
+            showReserve: false,
+            name:"",
+            phone:"",
+            totalPeople:"",
+            comments:""
         }
         this.handleReserve = this.handleReserve.bind(this);
     }
 
+
+    handleEmail(){
+
+let subject=this.state.name+" tiene una duda para Glam Hoteles";
+let message="nombre:"+this.state.name+"\n telefono:"+this.state.phone+"\n correo:"+this.state.email+"\n número de personas:"+totalPeople+"Fecha de reserva:"+this.state.startDate+"-"+this.state.endDate+"\n Comentarios:"+this.state.message;
+        
+        Meteor.call('sendEmail',"garzuxo@gmail.com", this.state.email, subject, message);
+    }
     handleReserve() {
 
 
         this.setState({ showReserve: !this.state.showReserve });
 
     }
-
+    handleOnChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
     componentDidMount() {
 
         this.state.reservePicker.push(
             <div id="reserve" key="reserve">
                 <div className="">
                     <label htmlFor="datepickerStart"><T>Common.contact.sinceDate</T></label>
-                    <DatePicker id="datepickerStart" className="form-control" value={this.state.startDate} required />
+                    <DatePicker id="datepickerStart" className="form-control" value={this.state.startDate}  onChange={this.handleOnChange.bind(this)} required />
                 </div>
                 <div className="">
                     <label htmlFor="datepickerEnd"><T>Common.contact.untilDate</T></label>
-                    <DatePicker id="datepickerEnd" className="form-control" value={this.state.endDate} required />
+                    <DatePicker id="datepickerEnd" className="form-control" value={this.state.endDate} onChange={this.handleOnChange.bind(this)} required />
                 </div>
             </div>
         );
@@ -56,15 +72,19 @@ export default class Contact extends Component {
                 <form>
                 <div className="form-group">
                         <label htmlFor="name"><T>Common.contact.name</T></label>
-                        <input  className="form-control" id="name"   required />
+                        <input  className="form-control" id="name"  value={this.state.name} onChange={this.handleOnChange.bind(this)} required />
+                          </div>
+                          <div className="form-group">
+                        <label htmlFor="phone"><T>Common.contact.phone</T></label>
+                        <input  className="form-control" id="phone"  value={this.state.phone} onChange={this.handleOnChange.bind(this)}  />
                           </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1"><T>Common.contact.email</T></label>
-                        <input type="email" className="form-control" id="exampleInputEmail1"  required />
+                        <input type="email" className="form-control" id="exampleInputEmail1" value={this.state.email} onChange={this.handleOnChange.bind(this)}  required />
                          </div>
                     <div className="form-group">
                         <label htmlFor="numberPeople"><T>Common.contact.totalPeople</T></label>
-                        <input type="number" className="form-control" id="numberPeople"  />
+                        <input type="number" className="form-control" id="numberPeople" value={this.state.totalPeople} onChange={this.handleOnChange.bind(this)}  />
                     </div>
                     <br></br>
                     <div className="form-check">
@@ -75,7 +95,7 @@ export default class Contact extends Component {
                     <br></br>  <br></br>
                     <div className="form-group">
                         <label htmlFor="exampleFormControlTextarea1"><T>Common.contact.comments</T></label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={this.state.coments} onChange={this.handleOnChange.bind(this)} ></textarea>
                     </div>
                     <button type="submit" className="btn btn-success"><T>Common.contact.send</T></button>
                 </form>
